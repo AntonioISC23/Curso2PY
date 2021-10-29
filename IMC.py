7ad503df92b58e675d1c73c484376b5456cfa1d5
Antonio <huguitar4@gmail.com>
2021-10-28 23:25:59 -0500

    Agregué impresión de resultados

--- IMC.py
+++ IMC.py
@@ -1,6 +1,11 @@
 
 
+from typing import SupportsRound
+
+
 masa=float(input("Ingresa tu masa en kilogramos: "))
 altura=float(input("Ingresa tu altura en metros: "))
 
 imc = masa/altura**2
+
+print("El indice de masa corporal es: "+str(imc)) 
